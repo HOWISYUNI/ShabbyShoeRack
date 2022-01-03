dd9c82d06d109295a1dacdb3c617deb1234e6306
HOWISYUNI <blissoooooo@gmail.com>
2022-01-04 00:53:53 +0900

    [실습] - 두수의 합 - 이진트리 출력하기 : 큐 활용

---
+++ DataStructureForAlgorithm/Lecture0/두수의합.py
@@ -0,0 +1,15 @@
+def twoSum(nums, target):
+    
+    for start in range(len(nums)-1):
+        for end in range(start, len(nums)):
+            if nums[start]+nums[end] == target:
+                return nums[start], nums[end]
+                
+    else:
+        return -1
+
+def main():
+    print(twoSum([2, 8, 19, 37, 4, 5], 12))
+
+if __name__ == "__main__":
+    main()
