1bec20d929eabb5d034bd6e95d5892ba4989134d
HOWISYUNI <blissoooooo@gmail.com>
2022-01-09 00:53:10 +0900

    [실습] - 최대부분합 정답 풀이 추가 - 1로만들기 다른 풀이 추가

--- DataStructureForAlgorithm/Lecture1/실습/1로만들기.py
+++ DataStructureForAlgorithm/Lecture1/실습/1로만들기.py
@@ -19,6 +19,22 @@ def convertTo1(num):
         
     return min(div2result+1, div3result+1, minusResult+1)
 
+# 2022.1.9 튜터 추천 풀이 추가
+# 1부터 1로 만드는 최소 값을 리스트에 계속 저장하면서 num에 도달
+def convertTo1(num):
+
+    d = [num for _ in range(num + 1)]
+    
+    d[1] = 0
+    
+    for i in range(2, num + 1):
+        if i % 3 == 0:
+            d[i] = min(d[i], d[i // 3] + 1)
+        if i % 2 == 0:
+            d[i] = min(d[i], d[i // 2] + 1)
+        d[i] = min(d[i], d[i - 1] + 1)
+    return d[num]
+
 def main():
     print(convertTo1(10))
 
--- DataStructureForAlgorithm/Lecture1/실습/가장큰부분합구하기.py
+++ DataStructureForAlgorithm/Lecture1/실습/가장큰부분합구하기.py
@@ -1,6 +1,6 @@
 import math
 
-# divide and conquer를 활용한 풀이(90점)
+# divide and conquer를 활용한 풀이(O(NlogN), 90점)
 def maxSubArray(nums):
     
     if len(nums) == 1:
@@ -26,6 +26,18 @@ def maxSubArray(nums):
     boundaryMax = leftMax + rightMax
     
     return max(maxSubArray(left), maxSubArray(right), boundaryMax)
+
+# O(N) 풀이법. 문제의 nums 중 양수는 하나라도 존재한다는 조건 활용
+def maxSubArray(nums): # O(N)의 풀이
+    s = 0
+    result = 0
+    for i in nums:
+        s += i
+        result = max(s, result)
+        if s < 0:
+            s = 0
+
+    return result
 
 def main():
     print(maxSubArray([-10, -7, 5, -7, 10, 5, -2, 17, -25, 1])) # 30이 리턴되어야 합니다
